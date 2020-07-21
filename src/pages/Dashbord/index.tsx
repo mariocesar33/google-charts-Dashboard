96eb02f92d044ba25d6fd6f2eb451e8a31944941
Mário César Silva <mcgato33@hotmail.com>
2020-07-21 02:52:05 -0100

    dados pronto

--- src/pages/Dashbord/index.tsx
+++ src/pages/Dashbord/index.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Chart from 'react-google-charts';
 
-import { Title} from './styles';
+import { Title, Repositories } from './styles';
+
 
 const Dashbord: React.FC = () => {
+  const title = "Quantidade de cadastros primeiro semestre";
+  const [repositories, setRepositories] = useState([
+    ['Mês', 'Quantidade'],
+    ['Janeiro', 33],
+    ['Feverreiro', 68],
+    ['Marco', 49],
+    ['Abril', 15],
+    ['Maio', 80],
+    ['Junho', 27]
+  ]);
+  
+
   return (
     <>
-      <Title>Hola Mundo!</Title>
+      <Title>Dashbord</Title>
+      <Repositories>
+        <Chart 
+          chartType={"PieChart"} 
+          data={repositories}
+          options={{ title: title }}
+        />
+      </Repositories>
+      
+        
+      
     </>
   );
 };
